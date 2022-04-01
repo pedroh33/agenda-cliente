93a915fdff4429a15cd45f246dcc50ad0022ad58
pedroh33 <75129379+pedroh33@users.noreply.github.com>
2022-04-01 11:12:20 -0300

    primer commit

---
+++ agenda-clientes/src/main/java/com/example/agendaclientes/AgendaClientesApplication.java
@@ -0,0 +1,13 @@
+package com.example.agendaclientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaClientesApplication.class, args);
+	}
+
+}
---
+++ agenda-clientes/src/test/java/com/example/agendaclientes/AgendaClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.agendaclientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
